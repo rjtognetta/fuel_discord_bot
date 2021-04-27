608bd4f59a85a3d1c5f98b5c5f3ba9d629e21228
Lucas Dias <llucasdias@icloud.com>
2021-04-27 21:09:42 +0200

    fixed return type calc_fuel

--- fuel.py
+++ fuel.py
@@ -7,4 +7,5 @@ def calc_fuel(stintTime, lapTime, fuelLap, tankCap):
     lapsAvg = stintTime / lapTime
     fuelTotal = lapsAvg * fuelLap
     pitTotal = fuelTotal / tankCap
-    print(f"Your calculated fuel is: %.0fL | Total Laps: %.0f | Pitstops Needed: %.0f" % (fuelTotal, lapsAvg, pitTotal))
+    calcFuelOut = "Your calculated fuel is: %.0fL | Total Laps: %.0f | Pitstops Needed: %.0f" % (fuelTotal, lapsAvg, pitTotal)
+    return calcFuelOut
