ee3c1bd1c5fb795b59fc56ee7cb3c8233810b83e
Rodrigo Tognetta <rjtognetta@me.com>
2021-04-28 18:05:43 +0200

    corrected round math

--- fuel.py
+++ fuel.py
@@ -7,6 +7,6 @@ import os
 def calc_fuel(stintTime, lapTime, fuelLap, tankCap):
     lapsAvg = stintTime / lapTime
     fuelTotal = lapsAvg * fuelLap
-    pitTotal = fuelTotal / tankCap
+    pitTotal = round((fuelTotal / tankCap) - 0.5)
     calcFuelOut = "Your calculated fuel is: %.0fL | Total Laps: %.0f | Pitstops Needed: %.0f" % (fuelTotal, lapsAvg, pitTotal)
     return calcFuelOut
